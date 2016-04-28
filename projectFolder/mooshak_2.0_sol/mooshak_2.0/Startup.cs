f6d9d10c450018c5bc1898d7427ab220a0398dd3
andrikarel <karelandri@gmail.com>
2016-04-28 11:18:56 +0000

    main project created, just default

---
+++ projectFolder/mooshak_2.0_sol/mooshak_2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mooshak_2._0.Startup))]
+namespace mooshak_2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
