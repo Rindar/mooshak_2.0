b745b5e954cac78b4fd6f1c901af30cde03c3be9
andrikarel <karelandri@gmail.com>
2016-05-05 14:18:19 +0000

    added a view to the course database in admin index

--- projectFolder/mooshak_2.0_sol/mooshak_2.0/Controllers/CourseController.cs
+++ projectFolder/mooshak_2.0_sol/mooshak_2.0/Controllers/CourseController.cs
@@ -7,6 +7,8 @@ using System.Web.Mvc;
 using mooshak_2._0.Services;
 using mooshak_2._0.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
+using mooshak_2._0.Models.Entities;
+using System.Security.Claims;
 
 namespace mooshak_2._0.Controllers
 {
@@ -24,6 +26,38 @@ namespace mooshak_2._0.Controllers
         {
             var viewModel = _service.GetCourseByID(id); // creates a viewmodel for the assignment
             return View(viewModel);
+        }
+
+
+        public ActionResult CourseIndex()
+        {
+            var db = new Dbcontext();
+            return View(db.courses.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult CourseIndex(Course list)
+        {
+        
+            if (ModelState.IsValid)
+            {
+
+                using (var db = new Dbcontext())
+                {
+                    string new_item = Request.Form["new_item"];
+                    var dbList = db.courses.Create();
+                    dbList.name = new_item;
+                    db.courses.Add(dbList);
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Incorrect format has been placed");
+
+            }
+            var listTable = new Dbcontext();
+            return View(listTable.courses.ToList());
         }
     }
 }
