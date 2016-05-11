6f23d74c5a4e5aaf49ab1480c887a1ee6819d7fe
Axel <axelbjornsson@gmail.com>
2016-05-11 15:34:34 +0000

    Byrjun á Error Handling

---
+++ projectFolder/mooshak_2.0_sol/mooshak_2.0/ErrorHandler/ErrorAttributeHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mooshak_2._0.ErrorHandler
+{
+    public class ErrorAttributeHandler : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            // HUGMYNDIR AF ERRORS 
+            // Finnur ekki síðu
+            // Submission virkar ekki
+            // 
+
+
+            base.OnException(filterContext);
+        }
+    }
+}
