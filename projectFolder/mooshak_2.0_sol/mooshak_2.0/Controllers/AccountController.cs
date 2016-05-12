ee44f3c215d93b8229a29f2bbe4f1d56b97d0b33
skuli <skuliarnarsson@gmail.com>
2016-05-12 09:45:46 +0000

    redirect fix

--- projectFolder/mooshak_2.0_sol/mooshak_2.0/Controllers/AccountController.cs
+++ projectFolder/mooshak_2.0_sol/mooshak_2.0/Controllers/AccountController.cs
@@ -170,7 +170,7 @@ namespace mooshak_2._0.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("UserList", "Admin");
                 }
                 AddErrors(result);
             }
