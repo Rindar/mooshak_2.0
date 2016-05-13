cdea88523e2113052b96fa3de6efa80341a48f58
andrikarel <karelandri@gmail.com>
2016-05-13 15:56:55 +0000

    finpussun

---
+++ projectFolder/mooshak_2.0_sol/mooshak_2.0/Scripts/CustomScripts.js
@@ -0,0 +1,29 @@
+﻿function deleteRow(id) {
+    var toDelete = confirm("Are you sure you want to delete this course?")
+    if (toDelete == true) {
+        window.location.assign("/Course/Delete/" + id);
+    }
+}
+
+function deleteUser(Id) {
+    var toDelete = confirm("Are you sure you want to delete this User?");
+    if (toDelete == true) {
+        console.log("message");
+        window.location.assign("/Admin/DeleteUser/" + Id);
+    }
+}
+
+function deleteFromCourse(id) {
+    window.location.assign("/Course/RemoveUser/" + id);
+}
+
+$(function helpIndex() {
+    $(".answer").hide();
+    $('#management li a').each(function () {
+        $(this).click(function () {
+            $(this).siblings(".answer").slideToggle();
+        });
+    });
+});
+
+
